1381ed28973a5f91784ebe41e69f069485d91ed3
Crystoppher <44615834+Crystoppher@users.noreply.github.com>
2022-01-21 15:53:07 -0300

    Atualizando
    Novos Atualizações em programas no repositório

--- prog02-calculoIMC/main.c
+++ prog02-calculoIMC/main.c
@@ -1,59 +1,62 @@
-/********************************************
- * 
- * NOME:      CRYSTOPPHER SIMÃ•ES TRINDADE
- * DATA:      24/10/2021
- * PROGRAMA:  REGISTRO DE ALUNOS - V1.0 
- *
- ********************************************/
+/* ============================================================================
+
+    PROJETO DE CALCULO DE IMC 
+    
+    Autor: Crystoppher Simões Trindade
+    Data:  21/01/2022
+
+============================================================================ */
 
 #include <stdio.h>
 #include <stdlib.h>
 
-int calculoIMC(int peso,int altura);
-
-int main() {
-   char opcao;
-   unsigned int peso,
-		altura; 
-
-   do
-   {
-   	printf("\n\t-------------------------------------\n");
-   	printf("\t---------- CALCULADORA IMC ----------\n");
-   	printf("\t-------------------------------------\n");
-     	printf("\t 1 - CONTINUAR                       \n");
-   	printf("\t 2 - SAIR                            \n");
-   	printf("\t-------------------------------------\n");
-   	//opcao = getchar();
-        scanf(" %c",&opcao);
-
-        switch(opcao)
-	{
-	 case '1': printf("\tSELECIONADA OPCAO NÂ° %c \n ",opcao);
-		 break;
-	 case '2': printf("\tSELECIONADA OPCAO NÂ° %c \n\n",opcao);
-		 exit(1);
-         default: printf("\tFAVOR SELECIONAR UMAS DAS OPCOES DEMOSTRADA NO MENU!\n\n");
+int main()
+{	
 	
-	}	
-
-   }while(opcao != '1');
+	/*ENTRADAS DE DADOS*/
+    float peso = 0.00,
+	      altura = 0.00,
+	      IMC    = 0.00;
+	      
+	unsigned short int TRATAMENTO_1 = 0;
+	unsigned short int TRATAMENTO_2 = 0;
 	
-   printf("\nINSIRA SEU PESO (KG): \n");
-   scanf("%d",&peso);
-   printf("INSIRA SUA ALTURA (M): \n");
-   scanf("%d",&altura);
-
-   calculoIMC(peso,altura);
-
-   printf("O VALOR DO SEU IMC EH  = %d \n\n",calculoIMC(peso,altura));
-
+	printf("Informe seu PESO: ");
+	TRATAMENTO_1 = scanf("%f",&peso);
+	
+	/*TRATAMENTO DE ENTRADA DE DADOS*/
+	if(!TRATAMENTO_1)
+	{ 
+	printf("Error! ");
+	exit(1);	
+	}else printf("OK");
+       
+	printf("\nInforme sua ALTURA: ");
+	TRATAMENTO_2 = scanf("%f",&altura);
+	
+	/*TRATAMENTO DE ENTRADA DE DADOS*/
+	if(!TRATAMENTO_2)
+	{ 
+	printf("Error! ");
+	exit(1);	
+	}else printf("OK");
+	
+	/*PROCESSAMENTO DE DADOS*/
+	IMC = peso / (altura * altura); 
+	
+	/*SAÍDA DE DADOS*/
+	printf("\n\nResultado so IMC: %.2f.",IMC);
+	putchar('\n');
+	
+	printf("==============================================\n");
+	printf("IMC\t\t\t  CLASSIFICACAO\t  GRAU\n");
+	printf("ENTRE18,5 E 24,9\t   NORMAL\t  0\n");
+	printf("ENTRE 25,0 E 29,9\t  SOBREPESO\t  I\n");
+	printf("ENTRE 30,0 E 39,9\t  OBESIDADE\t  II\n");
+	printf("MAIOR QUE 40,0\t\t     GRAVE\t  III\n");
+	printf("==============================================\n");
+	
+	
     return 0;
-}
- 
-int calculoIMC(int peso,int altura)
-{
-	return (peso/altura*altura);
-}
-  
-
+    
+} /* end main */
